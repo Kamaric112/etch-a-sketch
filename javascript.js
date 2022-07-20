f06914339106c37b0b50ca5ace46d4856723c9c3
Kamaric112 <ikasuzu123@gmail.com>
2022-07-20 21:50:16 +0700

    updated dynamic grid function and hover event

--- javascript.js
+++ javascript.js
@@ -1,14 +1,70 @@
 const container = document.querySelector('.container')
 
-function makeRow(rowNum) {
-    for (let i = 0 ; i < rowNum ; i++) {
-        let div = document.createElement('div')
-        for (let j = 0 ; j < rowNum; j++) {
-            let square = document.createElement('div')
-            square.setAttribute("class","box")
-            div.append(square)
-        }
-        container.appendChild(div);
-    }
+function test (num) {
+    const widthAndHeight = 350; //Width and Height of grid Container
+    const pixelSquare = (widthAndHeight/num)+'px';
 
-} makeRow(4) 
+
+    for (let i = 0 ; i < num ; i++) {
+        let row = document.createElement('div')
+        for (let j = 0 ; j < num ;j++) {
+            let squares = document.createElement('div')
+            squares.setAttribute("class","box")
+            row.appendChild(squares)
+            container.appendChild(row)
+            squares.style.height=pixelSquare
+            squares.style.width=pixelSquare
+            squares.style.margin = '0px';
+            squares.style.border ='1px solid rgba(179,179,179,1)';
+            squares.style.boxSizing = 'border-box';
+
+
+            // mouse enter event listener
+            // squares.addEventListener("mouseenter",function() {
+            //     squares.style.backgroundColor="red"
+            // })
+            // squares.addEventListener("mouseleave",function(e) {
+            //     squares.style.backgroundColor = 'white'
+            // })
+        }
+        
+        
+    }
+}
+test(4)
+
+
+// const btn = document.querySelector('#refresh')
+// btn.addEventListener('click',   )
+
+
+// // function userinput() {
+// //     // let x = prompt("enter grid size")
+// //     let x = 5
+// //     console.log(x)
+// //     test(x)
+// // }
+
+// // function reload() {
+// //     test(16)
+
+// // }
+
+
+function createGridSquares(pixel) {
+    const widthAndHeight = 350; //Width and Height of grid Container
+    const pixelSquare = (widthAndHeight/pixel)+'px';
+    const totalPixels = pixel*pixel;
+    for (let i = 1; i <= totalPixels; i++) {
+        let innerD = document.createElement('div');
+        innerD.classList.add('innerDivs');
+        container.appendChild(innerD);
+        innerD.style.backgroundColor = '#cbcfc2';
+        innerD.style.height = pixelSquare;
+        innerD.style.width = pixelSquare;
+        innerD.style.margin = '0px';
+        innerD.style.border ='1px solid rgba(179,179,179,1)';
+        innerD.style.boxSizing = 'border-box';
+    }
+}
+// createGridSquares(16)
